80ff29afadac4f3bb087795a939ae769d7746cf1
Roman Stravnyak <350loljke@gmail.com>
2020-02-02 15:39:58 +0700

    v0.0.1 start

---
+++ CardList.js
@@ -0,0 +1,39 @@
+class CardList {
+  
+  constructor(container, api) {
+    this.container = container;
+    this.api = api;
+  }
+
+  // добавление карточки
+  addCard = () => {
+    const popupCard = document.querySelector('.popup_add');
+    const popupCardForm = document.forms.add;
+    const cardName = popupCardForm.elements.name.value;
+    const cardLink = popupCardForm.elements.link.value;
+  
+    event.preventDefault();
+
+    const api = new Api(this.api);
+    
+    this.container.append( new Card(this.api).create(cardName, cardLink) );
+
+    api.loading(true, popupCard);
+    api.postCard(cardName, cardLink)         
+      .finally( () => {
+        api.loading(false, popupCard);
+        popupCard.classList.remove('popup_is-opened')
+      });   
+    popupCardForm.reset();
+    this.render();
+  }
+
+  // рендер массива с карточками через метод create класса Card
+  render() {
+    new Api(this.api).getCardArray()
+      .then(json => json.forEach(item => 
+        this.container.append( new Card(this.api).create( item.name, item.link, item.likes, item.owner._id, item._id ) ))
+      )  
+  }
+ 
+}
---
+++ apiData.js
@@ -0,0 +1,7 @@
+const apiData = {
+  baseUrl: 'http://95.216.175.5/cohort6',
+  headers: {
+    authorization: '539f9124-63cf-4162-a4d6-8525d0ac67b9',
+    'Content-Type': 'application/json'
+  }
+}
