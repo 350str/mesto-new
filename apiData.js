e686524a46fd142e2c9e8de2722689b3a0eab58c
Roman Stravnyak <350loljke@gmail.com>
2020-02-02 15:45:52 +0700

    v0.0.2 fixed server url

--- apiData.js
+++ apiData.js
@@ -1,5 +1,5 @@
 const apiData = {
-  baseUrl: 'http://95.216.175.5/cohort6',
+  baseUrl: 'http://praktikum.tk/cohort6',
   headers: {
     authorization: '539f9124-63cf-4162-a4d6-8525d0ac67b9',
     'Content-Type': 'application/json'
