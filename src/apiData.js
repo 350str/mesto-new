19ccb2f5207ffa4aac49c4d1caa4258a5d3103a2
Roman Stravnyak <350loljke@gmail.com>
2020-02-06 22:11:16 +0700

    Updates

--- src/apiData.js
+++
@@ -1,9 +0,0 @@
-const apiData = {
-  baseUrl: 'https://praktikum.tk/cohort6',
-  headers: {
-    authorization: '539f9124-63cf-4162-a4d6-8525d0ac67b9',
-    'Content-Type': 'application/json'
-  }
-}
-
-export default apiData;
--- src/errors.js
+++
@@ -1,7 +0,0 @@
-const errors = {
-    length: 'Должно быть от 2 до 30 символов',
-    value: 'Это обязательное поле',
-    URL: 'Здесь должна быть ссылка'
-}
-
-export default errors;
